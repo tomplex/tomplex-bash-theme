319f15a35c407b9c555980f44ea9d65cfcb76c8e
tom caruso <carusot42@gmail.com>
2017-09-21 00:55:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### tomplex bash theme
+
+A theme for your bash prompt.
+
+### installation
+
+With `bash-it`, clone into `.bash-it/themes` and edit your `BASH_IT_THEME` env variable in your `.bashrc` to read `tomplex`.
+
+Otherwise, simply clone the repo and add `source path/to/tomplex.theme.bash` to your bashrc.
+
---
+++ tomplex.theme.bash
@@ -0,0 +1,69 @@
+function timer_now {
+    date +%s%N
+}
+
+function timer_start {
+    timer_start=${timer_start:-$(timer_now)}
+}
+
+function timer_stop {
+    local delta_us=$((($(timer_now) - $timer_start) / 1000))
+    local us=$((delta_us % 1000))
+    local ms=$(((delta_us / 1000) % 1000))
+    local s=$(((delta_us / 1000000) % 60))
+    local m=$(((delta_us / 60000000) % 60))
+    local h=$((delta_us / 3600000000))
+    # Goal: always show around 3 digits of accuracy
+    if ((h > 0)); then timer_show=${h}h${m}m
+    elif ((m > 0)); then timer_show=${m}m${s}s
+    elif ((s >= 10)); then timer_show=${s}.$((ms / 100))s
+    elif ((s > 0)); then timer_show=${s}.$(printf %03d $ms)s
+    elif ((ms >= 100)); then timer_show=${ms}ms
+    elif ((ms > 0)); then timer_show=${ms}.$((us / 100))ms
+    else timer_show=${us}us
+    fi
+    unset timer_start
+}
+
+__check_virtualenv() {
+    [[ -n $VIRTUAL_ENV ]] && printf "($(basename $VIRTUAL_ENV)) "
+}
+
+set_prompt () {
+    Last_Command=$? # Must come first!
+    Blue='\[\e[01;34m\]'
+    White='\[\e[01;37m\]'
+    Red='\[\e[01;31m\]'
+    Green='\[\e[01;32m\]'
+    Reset='\[\e[00m\]'
+    FancyX='\342\234\227'
+    Checkmark='\342\234\223'
+
+    # Add a bright white exit status for the last command
+    PS1="$White\$? "
+    # If it was successful, print a green check mark. Otherwise, print
+    # a red X.
+    if [[ $Last_Command == 0 ]]; then
+        PS1+="$Green$Checkmark "
+    else
+        PS1+="$Red$FancyX "
+    fi
+
+    # Add the ellapsed time and current date
+    timer_stop
+    PS1+="($timer_show) $White\t "
+
+    # If root, just print the host in red. Otherwise, print the current user
+    # and host in green.
+    if [[ $EUID == 0 ]]; then
+        PS1+="$Red\\u${Blue}@\\h "
+    else
+        PS1+="${Blue}\\u@\\h "
+    fi
+    # Print the working directory and prompt marker in blue, and reset
+    # the text color to the default.
+    PS1+="$White$(__check_virtualenv)$Blue\\w\n\\\$$White "
+}
+
+trap 'timer_start' DEBUG
+safe_append_prompt_command set_prompt
